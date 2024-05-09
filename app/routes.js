2785f4fb96a1b2c579c53a79f277238890133cd1
Carlo Veo <carlo.veo@digital.justice.gov.uk>
2024-05-09 14:09:12 +0100

    create new session on launch

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,7 @@
 //
 
 const govukPrototypeKit = require('govuk-prototype-kit')
+const sessionDataDefaults = require('./data/session-data-defaults')
 const router = govukPrototypeKit.requests.setupRouter()
 
 router.post('/:prototypeVersion/next-court-date-select', function(req, res) {
@@ -940,7 +941,10 @@ router.post('/:prototypeVersion/next-court-appearance-complete', function(req, r
 
 router.get('/:prototypeVersion/launch-prototype', function(req, res) {
     const prototypeVersion = req.params.prototypeVersion
-    req.session.data.prototypeVersion = prototypeVersion
-    console.log("Launching prototype version: " + prototypeVersion)
+    req.session.regenerate(function(err) {
+        req.session.data = sessionDataDefaults
+        req.session.data.prototypeVersion = prototypeVersion
+        console.log("Launching prototype version: " + prototypeVersion)
         res.redirect(`/${prototypeVersion}/court-cases/`)
+    })
 })
